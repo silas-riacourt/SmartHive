bd02563c48602eeae9a18dc19f3aa3d1e75208e4
Silasssss <silasdu22@gmail.com>
2017-11-28 22:44:34 +0100

    ...

--- inc/db.php
+++ inc/db.php
@@ -1,4 +1,6 @@
 <?php
-$pdo = new PDO('mysql:dbname=web1;host=localhost', 'root', '');
+$host = "localhost";
+$dbname= "web1";
+$pdo = new PDO("mysql:dbname=$dbname;host=$host", "root", "");
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
