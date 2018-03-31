717466828e25fbf56155c8d3fd06c1ab2e1b4a9d
Silasssss <silasdu22@gmail.com>
2018-03-31 14:11:03 +0200

    remove useless folder

--- demo/inc/db.php
+++
@@ -1,9 +0,0 @@
-/* 
-    Author : Silas riacourt <silasdu22@gmail.com>
-*/ 
-<?php
-$host = "localhost";
-$dbname= "web1";
-$pdo = new PDO("mysql:dbname=$dbname;host=$host", "root", "");
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
--- demo/inc/footer.php
+++
@@ -1,6 +0,0 @@
- <footer>
-        <p>© 2017<a style="color:#0a93a6; text-decoration:none;" href="#"> SmartHive</a> site développé dans la cadre des PPE 2017-2018.</p>
-    </footer>
- </div>
-  </body>
-</html>
